470b0f8669f68e2aeab0f2dd77b4922707949333
XCarrel <Xavier.Carrel@cpnv.ch>
2020-02-20 10:53:14 +0100

    Dumb movie index page

---
+++ MovieFan/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFan.Models
+{
+    public class Movie
+    {
+        private string title;
+
+        public Movie(string title)
+        {
+            this.title = title;
+        }
+
+        public string Title { get => this.title; }
+    }
+}
