fba9ba012ccc8989e045fa2f166be32727e4b65a
XCarrel <Xavier.Carrel@cpnv.ch>
2020-02-20 13:40:36 +0100

    Hardcoded data done

--- MovieFan/Models/Movie.cs
+++ MovieFan/Models/Movie.cs
@@ -9,15 +9,19 @@ namespace MovieFan.Models
     {
         private int id;
         private string title;
+        private string synopsis;
 
-        public Movie(int id, string title)
+        public Movie(int id, string title, string synopsis)
         {
             this.id = id;
             this.title = title;
+            this.synopsis = synopsis;
         }
 
         public string Title { get => this.title; }
 
         public int Id { get => this.id; }
+
+        public string Synopsis { get => this.synopsis; set => this.synopsis = value; }
     }
 }
---
+++ MovieFan/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFan.Models
+{
+    public class User
+    {
+        private int id;
+        private string firstname;
+        private string lastname;
+        private bool isadmin;
+
+        public User(int id, string firstname, string lastname, bool isadmin)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.isadmin = isadmin;
+        }
+
+        public string Firstname { get => this.firstname; }
+        public string Lastname { get => this.lastname; }
+        public bool IsAdmin { get => this.isadmin; }
+        public int Id { get => this.id; } 
+    }
+}
