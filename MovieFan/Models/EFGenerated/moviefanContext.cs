54f2a8548db9649ef9fa0d7643f884de03460beb
XCarrel <Xavier.Carrel@cpnv.ch>
2020-02-24 12:55:34 +0100

    cleanup

--- MovieFan/Models/EFGenerated/moviefanContext.cs
+++ MovieFan/Models/EFGenerated/moviefanContext.cs
@@ -21,15 +21,6 @@ namespace MovieFan.Models
         public virtual DbSet<UserLikeMovie> UserLikeMovie { get; set; }
         public virtual DbSet<Users> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=moviefan;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Categories>(entity =>
