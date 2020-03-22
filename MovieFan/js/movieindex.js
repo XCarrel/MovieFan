5f6d600e0cdf30424f66fe991cbc220674bfdb7a
XCarrel <Xavier.Carrel@cpnv.ch>
2020-03-22 13:16:54 +0100

    Autosubmit form

---
+++ MovieFan/js/movieindex.js
@@ -0,0 +1,12 @@
+﻿// movieindex.js: js needed for the Index view
+// Author: XCL
+// March 2020
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Toggle Show/Edit movie
+    CategoryId.addEventListener('change', function () {
+        frmSelectCategory.submit()
+    })
+
+})
